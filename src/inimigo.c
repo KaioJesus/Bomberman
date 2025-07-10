a88a85f352a5928538113b298bd6b9e01fcc8153
DanielMuradas <danielmuradas1@gmail.com>
2025-07-10 20:41:09 -0300

    Update inimigo.c

--- src/inimigo.c
+++ src/inimigo.c
@@ -23,35 +23,41 @@ void InicializarInimigos(Game *game) {
 }
 
 // Função para atualizar movimento dos inimigos
-void AtualizarInimigos(Game *game) {
-    float tempoAtual = GetTime();
-
+void AtualizaInimigos(Game *game) {
     for (int i = 0; i < game->InimigoCount; i++) {
-        Inimigo *enemy = &game->enemies[i];
-
-        // Mudar direção a cada 2 segundos
-        if (tempoAtual - enemy->moveTimer > 2.0f) {
-            enemy->dir = (Direcao)(rand() % 4);
-            enemy->moveTimer = tempoAtual;
-        }
-
-        Posicao novaPos = enemy->pos;
-        switch (enemy->dir) {
-            case DIR_CIMA:    novaPos.y--; break;
-            case DIR_BAIXO:   novaPos.y++; break;
-            case DIR_ESQUERDA: novaPos.x--; break;
-            case DIR_DIREITA: novaPos.x++; break;
-        }
-
-        // Verifica se posição é livre
-        if (EhValidaPosicao(game, novaPos)) {
-            enemy->pos = novaPos;
-        } else {
-            // Se bater em parede, sorteia nova direção
-            enemy->dir = (Direcao)(rand() % 4);
-            enemy->moveTimer = tempoAtual;
-        }
-
+        game->enemies[i].moveTimer += GetFrameTime();
         
+        if (game->enemies[i].moveTimer >= 0.5f) {
+            game->enemies[i].moveTimer = 0.0f;
+            
+            // Tentar mover na direcao atual
+            Posicao novaPosicao = game->enemies[i].pos;
+            
+            switch (game->enemies[i].dir) {
+                case DIR_CIMA: novaPosicao.y--; break;
+                case DIR_BAIXO: novaPosicao.y++; break;
+                case DIR_ESQUERDA: novaPosicao.x--; break;
+                case DIR_DIREITA: novaPosicao.x++; break;
+            }
+            
+            if (EhValidaPosicao(game, novaPosicao)) {
+                // Verificar se não há outro inimigo na posição
+                bool occupied = false;
+                for (int j = 0; j < game->InimigoCount; j++) {
+                    if (j != i && verificaColisao(novaPosicao, game->enemies[j].pos)) {
+                        occupied = true;
+                        break;
+                    }
+                }
+                
+                if (!occupied) {
+                    game->enemies[i].pos = novaPosicao;
+                } else {
+                    game->enemies[i].dir = rand() % 4;
+                }
+            } else {
+                game->enemies[i].dir = rand() % 4;
+            }
+        }
     }
 }
